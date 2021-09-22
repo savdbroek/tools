d19335f0062619d7d992a87e1792262ea57d27bf
savdbroek <91181395+savdbroek@users.noreply.github.com>
2021-09-22 09:12:41 +0200

    Add files via upload

---
+++ gettools.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git clone https://github.com/itm4n/FullPowers
+git clone https://github.com/shelld3v/JSshell
+git clone https://github.com/trustedsec/social-engineer-toolkit
+git clone https://github.com/carlospolop/PEASS-ng 
