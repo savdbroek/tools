f4bb1b357b4be25e14840945086d17363019cd05
savdbroek <91181395+savdbroek@users.noreply.github.com>
2021-09-27 12:19:56 +0200

    Update gettools.sh
    Added xsser

--- gettools.sh
+++ gettools.sh
@@ -3,3 +3,4 @@ git clone https://github.com/itm4n/FullPowers
 git clone https://github.com/shelld3v/JSshell
 git clone https://github.com/trustedsec/social-engineer-toolkit
 git clone https://github.com/carlospolop/PEASS-ng 
+git clone https://github.com/epsylon/xsser
