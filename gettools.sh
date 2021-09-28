99558fb161a5238d202bdc6937a03493a276ad54
savdbroek <91181395+savdbroek@users.noreply.github.com>
2021-09-28 12:05:58 +0200

    Update gettools.sh

--- gettools.sh
+++ gettools.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 mkdir tools
 mkdir wordlists
-git clone https://github.com/itm4n/FullPowers tools
-git clone https://github.com/shelld3v/JSshell tools
-git clone https://github.com/trustedsec/social-engineer-toolkit tools
-git clone https://github.com/carlospolop/PEASS-ng tools
-git clone https://github.com/epsylon/xsser tools
-git clone https://github.com/danielmiessler/SecLists/blob/master/Discovery/Web-Content/common-and-dutch.txt wordlists
-git clone https://www.sjoerdlangkemper.nl/wordlists/linkedin-dutch-top-1134.txt wordlists
+git clone https://github.com/itm4n/FullPowers tools/fullpowers
+git clone https://github.com/shelld3v/JSshell tools/jsshell
+git clone https://github.com/trustedsec/social-engineer-toolkit tools/set
+git clone https://github.com/carlospolop/PEASS-ng tools/peass
+git clone https://github.com/epsylon/xsser tools/xsser
+git clone https://github.com/danielmiessler/SecLists/blob/master/Discovery/Web-Content/common-and-dutch.txt wordlists/dirbust
+git clone https://www.sjoerdlangkemper.nl/wordlists/linkedin-dutch-top-1134.txt wordlists/passwords
