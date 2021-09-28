ad1031e519c6fa5a32882d48ea31b5c6e387a1ee
savdbroek <91181395+savdbroek@users.noreply.github.com>
2021-09-28 12:01:07 +0200

    Update gettools.sh

--- gettools.sh
+++ gettools.sh
@@ -7,3 +7,4 @@ git clone https://github.com/trustedsec/social-engineer-toolkit tools
 git clone https://github.com/carlospolop/PEASS-ng tools
 git clone https://github.com/epsylon/xsser tools
 git clone https://github.com/danielmiessler/SecLists/blob/master/Discovery/Web-Content/common-and-dutch.txt wordlists
+git clone https://www.sjoerdlangkemper.nl/wordlists/linkedin-dutch-top-1134.txt wordlists
